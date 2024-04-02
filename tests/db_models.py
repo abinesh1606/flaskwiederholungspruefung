4955f1757d8bc0e6d111c69aa2aa6baf96763a2e
abinesh1606 <abinesh.gulasingam@gmail.com>
2024-04-02 10:41:37 +0200

    create columns  and lijes

--- tests/db_models.py
+++ tests/db_models.py
@@ -9,3 +9,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+    posts = db.relationship('Post', backref='author', passive_deletes=True)
+    likes = db.relationship('Like', backref='author', passive_deletes=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    
+    created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
+    
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
