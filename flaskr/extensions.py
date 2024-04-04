d9652c5760c7965c24a99c9650f8903984a975ed
abinesh1606 <abinesh.gulasingam@gmail.com>
2024-04-04 23:13:11 +0200

    few changes

--- flaskr/extensions.py
+++ flaskr/extensions.py
@@ -2,6 +2,7 @@ from flask_login import  LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 
+
 api = Api(
         title='Blog Post API',
         description='API for adding, updating and deleting blog post.',
