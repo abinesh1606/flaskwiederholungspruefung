e7ade7e4ec9e976ae63bbff1e2e3b86c5bb2d5fb
abinesh1606 <abinesh.gulasingam@gmail.com>
2024-04-04 19:23:03 +0200

    modified:   flaskr/__init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,37 +36,6 @@ def create_app(test_config=None):
 
     from .extensions import db,login_manager,api
     blueprint = Blueprint('api', __name__, url_prefix='/api')
-
-    api.init_app(blueprint)
-    from .resources import ns
-    api.add_namespace(ns)
     
-    db.init_app(app)
-    login_manager.init_app(app)
-    
-    from .db_models import User
-    
-    @login_manager.user_loader
-    def user_loader(user_id):
-        return User.query.get(int(user_id))
 
     
-    @login_manager.user_loader
-    def user_loader(user_id):
-        return User.query.get(int(user_id))
-
-    # apply the blueprints to the app
-    from . import auth
-    from . import blog
-
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
-    app.register_blueprint(blueprint)
-
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="index")
-
-    return app
