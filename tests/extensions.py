69e347f1e13fd1446e37abbbdc23a25d3e79151e
abinesh1606 <abinesh.gulasingam@gmail.com>
2024-04-04 18:12:20 +0200

    addition to extension class

--- tests/extensions.py
+++ tests/extensions.py
@@ -2,4 +2,10 @@ from flask_login import  LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 
-
+api = Api(
+        title='Blog Post API',
+        description='API for adding, updating and deleting blog post.',
+        doc='/docs/'
+    )
+db = SQLAlchemy()
+login_manager = LoginManager()
