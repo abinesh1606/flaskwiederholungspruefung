02585e8da83ab97d0062569416c55f7d928f47e1
abinesh1606 <abinesh.gulasingam@gmail.com>
2024-04-04 22:01:35 +0200

    i added the rest of api_models which got splitted accidently before

--- flaskr/api_models.py
+++ flaskr/api_models.py
@@ -10,3 +10,14 @@ post_model = api.model("Post", {
     "created":fields.DateTime,
     "author_id":fields.Integer,
 })
+post_input_model = api.model("PostInput", {
+    "title": fields.String(required=True, description="Post title"),
+    "body": fields.String(required=True, description="Post description"),
+    "author_id": fields.Integer(required=True, description="Author id"),
+})
+
+post_update_model = api.model("PostUpdate", {
+    "title": fields.String,
+    "body": fields.String,
+    "author_id": fields.Integer,
+})
